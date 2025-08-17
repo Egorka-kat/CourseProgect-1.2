faf7446e715b97232f5344bb524a13b74f723c7b
Катылёв Егор <egorkatylev@mail.ru>
2025-08-17 19:49:22 +0300

    Создание вспомогательного окна для ввода имени проекта

---
+++ CourseProgect 1.2/views/Windows/WindowForEnteringProjectName.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CourseProgect_1._2.views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowForEnteringProjectName.xaml
+    /// </summary>
+    public partial class WindowForEnteringProjectName : Window
+    {
+        public WindowForEnteringProjectName()
+        {
+            InitializeComponent();
+        }
+    }
+}
