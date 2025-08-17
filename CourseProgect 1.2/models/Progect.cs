23e8e838c041675cf686bcd38dfaf825162ff4b7
Катылёв Егор <egorkatylev@mail.ru>
2025-08-17 16:05:43 +0300

    Добавьте файлы проекта.

---
+++ CourseProgect 1.2/models/Progect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProgect_1._2.Models
+{
+    internal class Progect : INotifyPropertyChanged
+    {
+        public int id { get; set; }
+        private string name, path, last_call_date;
+        public string Name { get { return name; } set { name = value; OnPropertyChanged("Name"); } }
+        public string Path { get { return path; } set { path = value; OnPropertyChanged("Path"); } }
+        public string Last_call_date { get { return last_call_date; } set { last_call_date = value; OnPropertyChanged("Last_call_date"); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ CourseProgect 1.2/viewmodels/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CourseProgect_1._2.Infrastructure.Commands;
+using CourseProgect_1._2.ViewModels.Dase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CourseProgect_1._2.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _Title = "TextEditor";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+
+
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; set; }
+        private bool CanCloseApplicationCommandExecuted(object par) => true;
+        public void OnCloseApplicationCommandExecuted(object par) 
+        { 
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #endregion
+        public MainWindowViewModel() 
+        {
+            #region Команды
+
+            #region CloseApplicationCommand
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecuted);
+            #endregion
+
+            #endregion
+        }
+    }
+}
